b9a1fe585530dd03b9d0365c950a71735e675f97
Bryan Tan (Chen Zhengyu) <bryantanzy@gmail.com>
2020-10-08 19:04:15 +0800

    Completed all tests
    Class and method inheritance is improvable though

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,64 @@
+import math
+
 class Rectangle:
+    def __init__(self,width,height):
+        super().__init__()
+        self.width = width
+        self.height = height
+
+    def __repr__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
+
+    def set_width(self,width):
+        self.width = width
+    
+    def set_height(self,height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** 0.5
+
+    def get_picture(self):
+        output = ""
+        if (self.width > 50 or self.height > 50):
+            return "Too big for picture."
+        for x in range(self.height):
+            output += "*"*self.width
+            output += "\n"
+        return output
+
+    def get_amount_inside(self, shape):
+        return math.floor(self.height/shape.height) * math.floor(self.width/shape.width)
 
 
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side,side)
+        self.side = side
+        self.width = side
+        self.height = side
 
+    def __repr__(self):
+        return f"Square(side={self.side})"
 
-class Square:
+    def set_side(self, side):
+        self.side = side
+        self.width = side
+        self.height = side
+
+    def set_width(self, side):
+        self.side = side
+        super().set_width(side)
+        super().set_height(side)
+
+    def set_height(self, side):
+        self.side = side
+        super().set_width(side)
+        super().set_height(side)
+
